dfd8705012cf6d73abfd4c0402f03bf701933f7e
ariyan459 <ariyan.25bcs10314@sst.scaler.com>
2025-12-06 01:26:07 +0530

    Update notification handling in script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const enableBtn = document.getElementById("enable");
 const sendBtn = document.getElementById("send");
 
+if (!("Notification" in window)) {
+  alert("This browser does not support notifications.");
+}
 
 enableBtn.addEventListener("click", async () => {
   const permission = await Notification.requestPermission();
@@ -9,10 +12,9 @@ enableBtn.addEventListener("click", async () => {
     alert("Notifications Enabled!");
     sendBtn.disabled = false;
   } else {
-    alert("Please allow notifications to continue!");
+    alert("You blocked notifications!");
   }
 });
-
 
 sendBtn.addEventListener("click", () => {
   new Notification("👋 Hey there!", {
@@ -20,3 +22,4 @@ sendBtn.addEventListener("click", () => {
     icon: "https://cdn-icons-png.flaticon.com/512/1827/1827345.png"
   });
 });
+
